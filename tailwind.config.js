7b31f47ca32150f5f6662545f9a5226136daedc8
Ameng <fiqihz096@gmail.com>
2022-10-17 11:17:24 +0700

    Customize container configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js, jsx, ts, tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
     extend: {
       screens: {
         "2xl": "1320px",
